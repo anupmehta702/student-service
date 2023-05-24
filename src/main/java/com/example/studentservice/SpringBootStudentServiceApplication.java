641d047de07d24c7a6fd15ec2d878b62c34192b8
anupm <anupmehta702@gmail.com>
2023-05-24 10:44:49 +0530

    added deployedFrom env variable

--- src/main/java/com/example/studentservice/SpringBootStudentServiceApplication.java
+++ src/main/java/com/example/studentservice/SpringBootStudentServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.studentservice;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class SpringBootStudentServiceApplication {
 
+    @Value("${deployed.from}")
+    private String deployedFrom;
+
+
 	@RequestMapping(value = "/echoStudentName/{name}")
 	public String echoStudentName(@PathVariable(name = "name") String name) {
-		return "Hello  " + name + " Responsed on : " + new Date();
+		return "Hello  " + name + " Responsed on : " + new Date()+" and deployed from :"+deployedFrom;
 	}
 
 	@RequestMapping(value = "/getStudentDetails/{name}")
